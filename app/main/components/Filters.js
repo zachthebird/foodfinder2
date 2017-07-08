7bcb12af2efb2a43dff8b31046531a0f3223564d
Zach Bird <zachthebird@gmail.com>
2017-07-07 20:00:36 -0500

    styling sterff

--- app/main/components/Filters.js
+++ app/main/components/Filters.js
@@ -8,13 +8,23 @@ class Filters extends Component {
 
     
     render(){
+        const buttonClasses = {
+            
+        }
         return(
             <div className='panel panel-default col-lg-10 col-lg-offset-1'>
                 <div className='panel-heading text-center'>
                     <h2>Filters</h2>
                 </div>
                 <div className='panel-body text-center'>
-                    Clickable Filters belong here
+                    <div className='row'>
+                        <button onClick={()=>{addClass('active')}} className='btn filter-btn btn-success'>Open Now</button>
+                        <button onClick={()=>{addClass('active')}} className='btn filter-btn btn-success'>Take Out</button>
+                        <button onClick={()=>{addClass('active')}} className='btn filter-btn btn-success'>Mexican</button>
+                        <button onClick={()=>{addClass('active')}} className='btn filter-btn btn-success'>Asian</button>
+                        <button onClick={()=>{addClass('active')}} className='btn filter-btn btn-success'>American</button>
+                        <button onClick={()=>{addClass('active')}} className='btn filter-btn btn-success'>Serves Alcohol</button>
+                    </div>
                 </div>
             </div>
         )
