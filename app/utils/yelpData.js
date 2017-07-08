0e6b5564fd1c336cdbdeff49bff115feb024c4fd
epribs <ericpribramsky@gmail.com>
2017-07-08 10:51:38 -0500

    adding data

---
+++ app/utils/yelpData.js
@@ -0,0 +1,93 @@
+var yelpData = {
+  "total": 8228,
+  "businesses": [
+    {
+      "price": "$$",
+      "phone": "+1512555555",
+      "categories": [
+        {
+          "alias": "Brazillian"
+        }
+      ],
+      "name": "Boteco",
+      "url": "https://www.yelp.com/biz/boteco-austin-3",
+      "location": {
+        "city": "Austin",
+        "country": "US",
+        "address2": "",
+        "address3": "",
+        "state": "TX",
+        "address1": "1403 E 7th St",
+        "zip_code": "78702"
+      },
+      "distance": 1604.23,
+    },
+    {
+      "price": "$$",
+      "phone": "+14152520800",
+      "categories": [
+        {
+          "alias": "barbeque"
+        }
+      ],
+      "name": "Franklin Barbecue",
+      "url": "https://www.yelp.com/biz/franklin-barbecue-austin",
+      "location": {
+        "city": "Austin",
+        "country": "US",
+        "address2": "",
+        "address3": "",
+        "state": "TX",
+        "address1": "900 E 11th St",
+        "zip_code": "78702"
+      },
+      "distance": 1604.23,
+    },
+    {
+      "price": "$",
+      "phone": "+14152520800",
+      "categories": [
+        {
+          "alias": "Halal"
+        }
+      ],
+      "name": "Kismet Cafe",
+      "url": "https://www.yelp.com/biz/kismet-cafe-austin-2",
+      "location": {
+        "city": "Austin",
+        "country": "US",
+        "address2": "Ste 200",
+        "address3": "",
+        "state": "TX",
+        "address1": "1000 E 41st St",
+        "zip_code": "78751"
+      },
+      "distance": 1604.23
+    },
+    {
+      "price": "$",
+      "phone": "+14152520800",
+      "categories": [
+        {
+          "alias": "American"
+        }
+      ],
+      "name": "Salty Sow",
+      "url": "https://www.yelp.com/biz/salty-sow-austin",
+      "location": {
+        "city": "Austin",
+        "country": "US",
+        "address2": "",
+        "address3": "",
+        "state": "TX",
+        "address1": "1917 Manor Rd",
+        "zip_code": "78722"
+      },
+      "distance": 1604.23
+    }
+
+
+  ]
+}
+
+module.exports = yelpData;
